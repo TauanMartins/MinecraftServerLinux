3d880f2e00f3b1b0342417f184e333a43cc58d9b
Tauan Martins <83043097+TauanMartins@users.noreply.github.com>
2023-01-04 08:23:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice java -Xmx600M -Xms512M -XX:+UseG1GC -jar /home/ubuntu/McServerV1.10/mcserver/minecraft_server.jar nogui
+nice java -Xmx600M -Xms512M -XX:+UseG1GC -jar ./mcserver/minecraft_server.jar nogui
